124e6cbaff9dafc5140ddda33e7e755cd59d27a0
Nandan206 <167064786+Nandan206@users.noreply.github.com>
2024-05-05 20:41:33 +0800

    ML.py

--- ML.py
+++ ML.py
@@ -1,17 +1,9 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
+import numpy as np 
+import pandas as pd 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
@@ -290,4 +282,4 @@ sns.heatmap(conmat, annot=True, cbar=False)
 plt.title("Confusion Matrix")
 plt.show()
 
-print(classification_report(y_test,preds))
+print(classification_report(y_test,preds)
